254f6d923bbb17a34bc9d81222c2fe5e15b817ce
James McGauran <jamesmcgauran11@gmail.com>
2022-12-12 14:57:38 +0000

    Change name

--- org/example/Main.java
+++ org/example/Main.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Mobile Application Development with Java Sockets ");
+        System.out.println("Network Technologies Project - Java Sockets ");
 
         // initiate socket to connect to the server
         Socket socket = new Socket("localhost", 5000);
